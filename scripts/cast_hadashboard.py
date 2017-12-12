66b03e1cfd8714c7d25f969db612fb123ebab81a
kevinpanaro <panaro.kevin@gmail.com>
2017-12-11 23:25:28 -0500

    cast script for HADashboard

---
+++ scripts/cast_hadashboard.py
@@ -0,0 +1,23 @@
+import pychromecast
+import pychromecast.controllers.dashcast as dashcast
+import time
+
+casts = pychromecast.get_chromecasts()
+
+for cast in casts:
+    if cast.device.friendly_name == 'the future':
+        outcast = cast
+
+d = dashcast.DashCastController()
+outcast.register_handler(d)
+
+time.sleep(1)
+
+if not outcast.is_idle:
+    outcast.quit_app()
+    time.sleep(5)
+
+time.sleep(1)
+
+d.load_url('http://192.168.0.7:5050/HA')
+
